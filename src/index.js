0de613cf927ef754a82e6c56830ed444886b9b30
georgekara13 <george_kara_13@hotmail.com>
2019-11-12 23:31:14 +0200

    Some basic components & fonts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './routes'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+  <BrowserRouter>
+    <Routes/>
+  </BrowserRouter>
+)
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
