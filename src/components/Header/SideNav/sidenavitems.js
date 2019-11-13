503b9c065fce43c913e1001b7ef4630e16041623
georgekara13 <george_kara_13@hotmail.com>
2019-11-14 01:00:57 +0200

    move comment to top

--- src/components/Header/SideNav/sidenavitems.js
+++ src/components/Header/SideNav/sidenavitems.js
@@ -4,14 +4,14 @@ import FontAwesome from 'react-fontawesome'
 import style from './sidenav.module.css'
 import items from './itemsconfig'
 
-
+//TODO consume css class from itemconfig
 const SideNavItems = () => {
 
   const showItems = () => {
       return items.map((item,i) => {
           console.log(item.type);
           return (
-            <div key={i} className={style.option}> //TODO consume css class from itemconfig
+            <div key={i} className={style.option}>
               <Link to={item.link}><FontAwesome name={item.icon}/>{item.text}</Link>
             </div>
           )
